395870a62afd98b9d671553ee6306f9e00f1b8cb
LZ <113305177+lzajota@users.noreply.github.com>
2023-09-12 15:50:35 +0100

    geolocation rm/img preview chnage

--- index.js
+++ index.js
@@ -198,72 +198,72 @@ window.onload = () => {
 // WEATHER API
 // WEATHER API
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+//         console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 
-        console.log(latitude)
+//         console.log(latitude)
 
-        let weather = {
-            apiKey: "e960a1c2edec148280ede019ca0e60b9",
-            fetchWeather: function (city) {
-                fetch(
-                    "https://api.openweathermap.org/data/2.5/weather?q=" +
-                    city +
-                    "&units=metric&appid=" +
-                    this.apiKey
-                )
-                    .then((response) => {
-                        if (!response.ok) {
-                            alert("No weather found.");
-                            throw new Error("No weather found.");
-                        }
-                        return response.json();
-                    })
-                    .then((data) => this.displayWeather(data));
-            },
-            displayWeather: function (data) {
-                const { name } = data;
-                const { icon, description } = data.weather[0];
-                const { temp, humidity } = data.main;
-                const { speed } = data.wind;
-                document.querySelector(".city").innerText = "Weather in " + name;
-                document.querySelector(".icon").src =
-                    "https://openweathermap.org/img/wn/" + icon + ".png";
-                document.querySelector(".description").innerText = description;
-                document.querySelector(".temp").innerText = temp + "°C";
-                document.querySelector(".humidity").innerText =
-                    "Humidity: " + humidity + "%";
-                document.querySelector(".wind").innerText =
-                    "Wind speed: " + speed + " km/h";
-                document.querySelector(".weather").classList.remove("loading");
-                // document.querySelector(".card-weather").style.backgroundImage =
-                // "url('https://source.unsplash.com/1600x900/?" + description + "')";
-            },
-            search: function () {
-                this.fetchWeather(document.querySelector(".weather-search-bar").value);
-            },
-        };
-
-        document.querySelector(".weather-search button").addEventListener("click", function () {
-            weather.search();
-        });
-
-        document
-            .querySelector(".weather-search-bar")
-            .addEventListener("keyup", function (event) {
-                if (event.key == "Enter") {
-                    weather.search();
+let weather = {
+    apiKey: "e960a1c2edec148280ede019ca0e60b9",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
                 }
-            });
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        // document.querySelector(".card-weather").style.backgroundImage =
+        // "url('https://source.unsplash.com/1600x900/?" + description + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".weather-search-bar").value);
+    },
+};
 
-        weather.fetchWeather("London");
+document.querySelector(".weather-search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document
+    .querySelector(".weather-search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
     });
-} else {
-    console.log("Geolocation is not supported by this browser.");
-}
+
+weather.fetchWeather("London");
+//     });
+// } else {
+//     console.log("Geolocation is not supported by this browser.");
+// }
 
 
 
